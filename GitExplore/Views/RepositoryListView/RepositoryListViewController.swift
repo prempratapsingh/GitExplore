403161918c65cbb001c5b43f47c171faad56609b
Prem Pratap Singh <prem.pratap@xparrow.com>
2020-12-24 21:13:52 +0530

    Add initial project structure with view placeholders, folder structures, etc

---
+++ GitExplore/Views/RepositoryListView/RepositoryListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RepositoryListViewController.swift
+//  GitExplore
+//
+//  Created by Prem Pratap Singh on 24/12/20.
+//
+
+import UIKit
+
+class RepositoryListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .orange
+    }
+}
+
